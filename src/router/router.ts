24d254bd0e8266048794e618085d693ca782c660
wyswill <1151373571@qq.com>
2020-05-06 18:49:32 +0800

    是的

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,8 +1,14 @@
 import * as express from "express";
 
+const mock = require("mockjs");
 const router = express.Router();
 router.get("/", (req: any, res: any) => {
-  res.send("ok");
+  const data: string = mock.mock({
+    "list|1-10": [{
+      "id|+1": 1
+    }]
+  });
+  res.send(data);
 });
 
 module.exports = router;
